e25b5a31cea2627b3b008ea313d86bf1159b6ebe
Jesse Klotz <jesseklotz2306@gmail.com>
2021-12-09 21:01:14 +0100

    Update pages

---
+++ pages/contact.tsx
@@ -0,0 +1,17 @@
+import Footer from "@components/Footer";
+import Header from "@components/Header";
+import React from "react";
+
+interface contactProps {}
+
+export const contact = (props: contactProps) => {
+  return (
+    <div>
+      <Header />
+      <main></main>
+      <Footer />
+    </div>
+  );
+};
+
+export default contact;
---
+++ pages/statistics.tsx
@@ -0,0 +1,17 @@
+import Footer from "@components/Footer";
+import Header from "@components/Header";
+import React from "react";
+
+interface statisticsProps {}
+
+export const statistics = (props: statisticsProps) => {
+  return (
+    <div>
+      <Header />
+      <main></main>
+      <Footer />
+    </div>
+  );
+};
+
+export default statistics;
