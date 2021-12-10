249293e06301bae25b6b245759508ea5d7866813
Jesse <jesseklotz@live.de>
2021-12-10 13:55:44 +0100

    Remove font optional

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,7 +6,7 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <link
-            href="https://fonts.googleapis.com/css2?family=Sarabun&family=Bebas+Neue&display=optional"
+            href="https://fonts.googleapis.com/css2?family=Sarabun&family=Bebas+Neue"
             rel="stylesheet"
           />
         </Head>
