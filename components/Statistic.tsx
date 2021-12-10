be74b35bdcfce3bdb85e17d59bc36a1df6481b30
Jesse <jesseklotz@live.de>
2021-12-10 14:58:05 +0100

    Add Statistic Comps

---
+++ components/Statistic.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+interface StatisticProps {
+  title: string;
+  description: string;
+  graphic: string;
+  right?: boolean;
+}
+
+export const Statistic = (props: StatisticProps) => {
+  return (
+    <div className="relative flex flex-col-reverse px-4 py-16 pt-16 mx-auto lg:flex-row lg:py-0 sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8">
+      <div
+        className={`w-full max-w-xl px-4 mx-auto ${
+          props.right ? "lg:order-last" : ""
+        } md:px-0 lg:px-8 lg:py-20 lg:w-1/2 lg:max-w-screen-xl`}
+      >
+        <div className="mb-0 lg:max-w-lg lg:pr-8 xl:pr-6">
+          <h2 className="mb-5 font-sans text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl sm:leading-none ">
+            {props.title}
+          </h2>
+          <p className="mb-5 text-base text-gray-700 md:text-lg ">
+            {props.description}
+          </p>
+        </div>
+      </div>
+      <div className="w-full max-w-xl px-4 mx-auto mb-6 md:px-0 lg:pl-8 lg:pr-0 lg:mb-0 lg:mx-0 lg:w-1/2 lg:max-w-full xl:px-0">
+        <img
+          className="object-cover w-full h-56 rounded shadow-lg lg:rounded-none lg:shadow-none md:h-96 lg:h-full"
+          src={props.graphic}
+          alt=""
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Statistic;
--- pages/statistics.tsx
+++ pages/statistics.tsx
@@ -1,5 +1,6 @@
 import Footer from "@components/Footer";
 import Header from "@components/Header";
+import Statistic from "@components/Statistic";
 import React from "react";
 
 interface statisticsProps {}
@@ -8,7 +9,23 @@ export const statistics = (props: statisticsProps) => {
   return (
     <div>
       <Header />
-      <main></main>
+      <main>
+        <Statistic
+          title="The quick, brown fox jumps over a lazy dog"
+          description="Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+          accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
+          quae. explicabo."
+          graphic="https://images.pexels.com/photos/3184360/pexels-photo-3184360.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;h=750&amp;w=1260"
+        />
+        <Statistic
+          title="The quick, brown fox jumps over a lazy dog"
+          description="Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+          accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
+          quae. explicabo."
+          graphic="https://images.pexels.com/photos/3184360/pexels-photo-3184360.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;h=750&amp;w=1260"
+          right
+        />
+      </main>
       <Footer />
     </div>
   );
