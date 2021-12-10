bf6c4840dc5ad9f085a020a3512cee4d210448b1
Jesse <jesseklotz@live.de>
2021-12-10 18:07:20 +0100

    Fix mobile resp

--- components/WhySection.tsx
+++ components/WhySection.tsx
@@ -1,8 +1,8 @@
 export const ContentLeft = () => {
   return (
-    <div className="relative flex flex-col py-0 lg:pt-0 lg:flex-col lg:pb-0">
-      <img src="/why.svg" alt="" className="w-full" />
-      <div className="absolute bottom-0 left-0 right-0 flex flex-col items-center w-full max-w-xl px-4 mx-auto text-center md:px-0 lg:px-8 lg:max-w-screen-xl">
+    <div className="relative flex flex-col py-0 lg:pt-0 lg:flex-col lg:pb-0 bg-primary">
+      <img src="/why.svg" alt="" className="w-full bg-meatureWhite" />
+      <div className="bottom-0 left-0 right-0 flex flex-col items-center w-full max-w-xl px-4 mx-auto text-center md:absolute md:px-0 lg:px-8 lg:max-w-screen-xl">
         <div className="mb-16 lg:mb-11 lg:max-w-lg lg:pr-5">
           <h2 className="mb-5 font-sans text-3xl font-bold tracking-tight text-primaryText sm:text-4xl sm:leading-none md:text-center">
             Why are we
