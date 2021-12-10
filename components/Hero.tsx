43e0bab5008cb3f8e0f6f9fe07cd855ed52b7a06
Himyu <max_b98@hotmail.de>
2021-12-10 16:55:22 +0100

    change header

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -3,34 +3,14 @@ export const Hero = () => {
     <div className="relative flex flex-col-reverse py-16 lg:pt-0 lg:flex-col lg:pb-0 bg-hero-pattern bg-cover">
       <div className="relative flex flex-col items-center text-center w-full max-w-xl px-4 mx-auto md:px-0 lg:px-8 lg:max-w-screen-xl">
         <div className="mb-16 lg:my-40 lg:max-w-lg lg:pr-5">
-          <h2 className="mb-5 font-sans text-3xl font-bold tracking-tight text-white sm:text-4xl sm:leading-none">
+          <h1 className="mb-5 font-sans text-5xl font-bold tracking-tight text-primaryText sm:text-5xl sm:leading-none">
             Meature
             <br className="hidden md:block" />
             The future{" "}
-            <span className="inline-block text-deep-purple-accent-400">
+            <span className="inline-block text-primary">
               of Meat
             </span>
-          </h2>
-          <p className="pr-5 mb-5 text-base text-white md:text-lg">
-            Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-            accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
-            quae. explicabo.
-          </p>
-          <div className="flex items-center">
-            <a
-              href="/"
-              className="inline-flex items-center justify-center h-12 px-6 ml-auto mr-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-deep-purple-accent-400 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none"
-            >
-              Get started
-            </a>
-            <a
-              href="/"
-              aria-label=""
-              className="inline-flex items-center font-semibold text-white mr-auto transition-colors duration-200 hover:text-deep-purple-accent-700"
-            >
-              Learn more
-            </a>
-          </div>
+          </h1>
         </div>
       </div>
     </div>
