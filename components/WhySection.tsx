db25b19953508ad0b241af89e0194a187c9e0ada
Jesse <jesseklotz@live.de>
2021-12-10 19:41:05 +0100

    Fix Footer

--- components/WhySection.tsx
+++ components/WhySection.tsx
@@ -2,12 +2,11 @@ export const ContentLeft = () => {
   return (
     <div className="relative flex flex-col py-0 lg:pt-0 lg:flex-col lg:pb-0 bg-primary">
       <img src="/why.svg" alt="" className="w-full bg-meatureWhite" />
-      <div className="bottom-0 left-0 right-0 flex flex-col items-center w-full max-w-xl px-4 mx-auto text-center md:px-0 lg:px-8 lg:max-w-screen-xl">
+      <div className="left-0 right-0 flex flex-col items-center w-full max-w-xl px-4 mx-auto text-center -bottom-20 md:absolute md:px-0 lg:px-8 lg:max-w-screen-xl">
         <div className="mb-16 lg:mb-11 lg:max-w-lg lg:pr-5">
           <h2 className="mb-5 font-sans text-3xl font-bold tracking-tight text-primaryText sm:text-4xl sm:leading-none md:text-center">
             Why are we
-            <br className="hidden md:block" />
-            doing This?
+            <br className="hidden md:block" /> doing This?
           </h2>
           <p className="mb-5 text-base text-secondaryText md:text-lg md:text-center">
             Sed ut perspiciatis unde omnis iste natus error sit voluptatem
--- pages/index.tsx
+++ pages/index.tsx
@@ -23,7 +23,7 @@ const Home: NextPage = () => {
         <WhySection />
       </main>
 
-      <Footer />
+      <Footer index />
     </div>
   );
 };
