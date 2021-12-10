c2d8d6cca8ec28cc2419b92699f529a082b11f0c
Jesse <jesseklotz@live.de>
2021-12-10 17:25:43 +0100

    Add Footer background color

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export const Footer = () => {
   return (
-    <div className="px-4 pt-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 relative z-1">
+    <div className="relative px-4 pt-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 z-1 bg-primary">
       <div className="grid gap-10 row-gap-6 mb-8 sm:grid-cols-2 lg:grid-cols-4">
         <div className="sm:col-span-2">
           <a
